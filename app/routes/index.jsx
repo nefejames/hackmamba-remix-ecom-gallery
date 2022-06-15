a14267d764abfa8028066e1c796eb8e10487d966
Emadamerho-Atori Nefe <nefejames@yahoo.com>
2022-06-15 07:09:20 +0100

    commited

---
+++ app/routes/index.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+
+export default function Home() {
+  useEffect(() => {
+    const productGallery = cloudinary.galleryWidget({
+      container: "#product-gallery",
+      cloudName: "nefejames",
+      mediaAssets: [
+        { tag: "dog-picture", mediaType: "image" },
+        { tag: "dog-video", mediaType: "video" },
+      ],
+    });
+
+    productGallery.render();
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>Remix Ecommerce Product Gallery</h1>
+      <div className="gallery-container">
+        <div id="product-gallery"></div>
+      </div>
+    </div>
+  );
+}
